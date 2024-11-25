366a5678d70e760f799e8e5576da80349f6c2031
uneeb-ali <243622@students.au.edu.pk>
2024-11-25 14:41:59 +0500

    comment in  program.cpp

--- program.cpp
+++ program.cpp
@@ -1,13 +1,14 @@
+//heading file 
 #include<iostream>
 using nameapace std;
 int main()
-{ int a,b,ans;
+{ int a,b,ans;  // variable declaration 
 char op;
-cout<<"enter two number  "<<endl;
+cout<<"enter two number  "<<endl;     // taking valuess
 cin>>a>>b;
-cout<<"enter opertaor   "<<endl;
+cout<<"enter opertaor   "<<endl;     //taking opeator as input 
 cin>>op;
-switch (op)
+switch (op)               // using switch case to make calculators
 {  
 case '-':{ ans=a-b;
 cout<<ans;}
